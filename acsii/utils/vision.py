f95d5c0f46bc5467d9eb5608cb6a3c406f20f8d6
Sadra Yahyapour <lnxpylnxpy@gmail.com>
2023-04-05 21:26:56 +0330

    shifting feature replaced with cv resizing method

--- acsii/utils/vision.py
+++ acsii/utils/vision.py
@@ -3,6 +3,8 @@ from typing import IO
 import cv2
 from numpy import ndarray
 
+from os import get_terminal_size
+
 RANGES = {
     0: '-',
     255: '#',
@@ -16,13 +18,15 @@ class ImageCoder:
 
     # TODO: add docstrings to methods
 
-    def __init__(self, image: IO) -> None:
+    def __init__(self, image: IO, size: tuple = get_terminal_size()) -> None:
         """
         Args:
             image(IO): path to image file
+            size(tuple): image size (it fits to terminal size by default)
         """
 
         self.image: ndarray = cv2.imread(str(image), cv2.IMREAD_GRAYSCALE)
+        self.image = cv2.resize(self.image, size)
         _, self.image = cv2.threshold(self.image, 128, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
 
         # for caching purposes in calculate_size
@@ -30,13 +34,9 @@ class ImageCoder:
         self.width = None
         self.height = None
 
-    def serialize(self, height_shift: int = 1, width_shift: int = 1) -> list:
+    def serialize(self) -> list:
         """
-        Turns the entered image into an ASCII matrix based on the defined ranges
-
-        Args:
-            height_shift(int): the amount shift you want to reduce from the matrix height
-            width_shift(int): the amount shift you want to reduce from the matrix width
+        Turns the entered image into an ASCII matrix
 
         Returns:
             list: serialized image into ASCII
@@ -44,9 +44,9 @@ class ImageCoder:
 
         serialized: list = list()
         for line in self.image:
-            serialized.append([RANGES[i] for i in line[::width_shift]])
+            serialized.append([RANGES[i] for i in line])
 
-        self.serialized_context = serialized[::height_shift]
+        self.serialized_context = serialized
         return self.serialized_context
 
     def calculate_size(self) -> None:
