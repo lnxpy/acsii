c28368286e84eef71b79ab865e83ab4833a74bd5
Sadra Yahyapour <lnxpylnxpy@gmail.com>
2023-04-07 03:05:44 +0330

    serializer method created for converting the density

---
+++ acsii/utils/serializer.py
@@ -0,0 +1,17 @@
+from acsii.constants.patterns import BASE_PATTERN
+
+
+def serialize(density: int, typeset: str) -> str:
+    """
+    Serializes the density value converts it into a character.
+    Args:
+        typeset: ASCII characters type set
+        density: pixel density value between 1 and 255
+
+    Returns:
+        a character corresponding to the density value
+    """
+    print(typeset)
+    print(len(BASE_PATTERN[typeset]))
+
+    return BASE_PATTERN[typeset][density * len(BASE_PATTERN[typeset]) // 256]
