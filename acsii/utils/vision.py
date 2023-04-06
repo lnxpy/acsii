2e3264efc5ea91b120d42c7e46c78accb7ad5697
Sadra Yahyapour <lnxpylnxpy@gmail.com>
2023-04-07 03:03:28 +0330

    typeset attribute added to Encoder

--- acsii/utils/vision.py
+++ acsii/utils/vision.py
@@ -5,46 +5,41 @@ from numpy import ndarray
 
 from os import get_terminal_size
 
-RANGES = {
-    0: '-',
-    255: '#',
-}
+from acsii.utils.serializer import serialize
 
 
-class ImageCoder:
+class Encoder:
     """
-    worker interface between open-cv and ascii art making
+    Image encoder interface between open-cv and ascii art making
     """
 
-    # TODO: add docstrings to methods
-
-    def __init__(self, image: IO, size: tuple = get_terminal_size()) -> None:
+    def __init__(self, image: IO, typeset: str, size: tuple = get_terminal_size()) -> None:
         """
         Args:
-            image(IO): path to image file
-            size(tuple): image size (it fits to terminal size by default)
+            image: path to image file
+            typeset: ASCII characters type set
+            size: image size (it fits to terminal size by default)
         """
 
         self.image: ndarray = cv2.imread(str(image), cv2.IMREAD_GRAYSCALE)
         self.image = cv2.resize(self.image, size)
-        _, self.image = cv2.threshold(self.image, 128, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
 
         # for caching purposes in calculate_size
         self.serialized_context: list = list()
+        self.typeset = typeset
         self.width = None
         self.height = None
 
     def serialize(self) -> list:
         """
         Turns the entered image into an ASCII matrix
-
         Returns:
-            list: serialized image into ASCII
+            a matrix that contains the serialized columns of the image
         """
 
         serialized: list = list()
         for line in self.image:
-            serialized.append([RANGES[i] for i in line])
+            serialized.append([serialize(i, self.typeset) for i in line])
 
         self.serialized_context = serialized
         return self.serialized_context
