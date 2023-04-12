d74976d065189075fb1ace24cdf56bc558b68272
Sadra Yahyapour <lnxpylnxpy@gmail.com>
2023-04-12 23:22:08 +0330

    information about clips removed

--- acsii/constants/information.py
+++ acsii/constants/information.py
@@ -1,7 +1,6 @@
 # whole package information
 
-APPLICATION_DESCRIPTION = '''ACSII (not ASCII) is an image processing project that is capable of turning real images into ASCII arts.
-With the provided arguments, you can turn small clips into ASCII clips as well.'''
+APPLICATION_DESCRIPTION = 'ACSII (not ASCII) is an image processing project that is capable of turning real images into ASCII arts.'
 
 EPILOG_DESCRIPTION = 'This project is made for learning and college work purposes.'
 
