90ca950f339a3d410d62017a5c53e245f2c9d7b2
Sadra Yahyapour <lnxpylnxpy@gmail.com>
2023-07-19 05:08:10 +0330

    syms-inverted pattern added

--- acsii/constants/patterns.py
+++ acsii/constants/patterns.py
@@ -2,5 +2,6 @@ BASE_PATTERN = {
     "nums": " 71326508",
     "alphs": " vLTYFZURQW",
     "syms": " .:-=+*#%@",
+    "syms-inverted": " .:-=+*#%@"[::-1],
     "syms2": " .'`^\",:;Il!i><~+_-?][}{1)(|\\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$",
 }
